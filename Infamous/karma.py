611c01a172bbb0dcabaccfe882a045b3d7daaad5
arfin03 <156934678+arfin03@users.noreply.github.com>
2024-03-20 05:54:28 +0700

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -10,36 +10,36 @@ from Mikobot import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT
 
 # <============================================== CONSTANTS =========================================================>
 START_IMG = [
-    "https://telegra.ph/file/ca4f2e351a1725836bacc.jpg",   
-    "https://telegra.ph/file/03b143d9cff2b983b9aa3.jpg",
-    "https://telegra.ph/file/433a3b03efdf883ca626a.jpg",
-    "https://telegra.ph/file/aea4050cd9a14c4600437.jpg",
-    "https://telegra.ph/file/6efe8e36b932aef609162.jpg",
-    "https://telegra.ph/file/33e4abe360a62fe617cad.jpg",
-    "https://telegra.ph/file/8f93af0434fceb5b9f1df.jpg",
-    "https://telegra.ph/file/14ad1d194740e3cf0ff32.jpg",
+    "https://telegra.ph/file/40b93b46642124605e678.jpg",
+    "https://telegra.ph/file/01a2e0cd1b9d03808c546.jpg",
+    "https://telegra.ph/file/ed4385c26dcf6de70543f.jpg",
+    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
+    "https://telegra.ph/file/cce9038f6a9b88eb409b5.jpg",
+    "https://telegra.ph/file/262c86393730a609cdade.jpg",
+    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
 ]
 
-HEY_IMG = "https://telegra.ph/file/433a3b03efdf883ca626a.jpg"
+HEY_IMG = "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg"
 
 ALIVE_ANIMATION = [
-    "https://telegra.ph/file/255a4d983cbcb6f2714c3.jpg",
-    "https://telegra.ph/file/e1e68e5a1c1899997ce7f.jpg",
-    "https://telegra.ph/file/ed4385c26dcf6de70543f.jpg",
-    "https://telegra.ph/file/4734d09fa1937300e8aba.jpg",
-    "https://telegra.ph/file/255a4d983cbcb6f2714c3.jpg",
-    "https://telegra.ph/file/e1e68e5a1c1899997ce7f.jpg",
-    "https://telegra.ph/file/4734d09fa1937300e8aba.jpg",
+    "https://telegra.ph//file/f9e2b9cdd9324fc39970a.mp4",
+    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
+    "https://telegra.ph//file/c4c2759c5fc04cefd207a.mp4",
+    "https://telegra.ph//file/b1fa6609b1c4807255927.mp4",
+    "https://telegra.ph//file/f3c7147da6511fbe27c25.mp4",
+    "https://telegra.ph//file/39071b73c02e3ff5945ca.mp4",
+    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
+    "https://telegra.ph//file/6efdd8e28756bc2f6e53e.mp4",
 ]
 
-FIRST_PART_TEXT = "✨ *Hey* `{}` . . ."
+FIRST_PART_TEXT = "✨ *ʜᴇʟʟᴏ* `{}` . . ."
 
-PM_START_TEXT = "✨ *im Fubuki, A new generation group manager bot with extreme futures*"
+PM_START_TEXT = "✨ *ɪ ᴀᴍ ᴍɪᴋᴏ, ᴀ ɢᴇɴꜱʜɪɴ ɪᴍᴘᴀᴄᴛ ᴛʜᴇᴍᴇᴅ ʀᴏʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ ꜱᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ*"
 
 START_BTN = [
     [
         InlineKeyboardButton(
-            text="➕ ADD ME ➕",
+            text="⇦ ADD ME ⇨",
             url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
         ),
     ],
@@ -49,10 +49,10 @@ START_BTN = [
     [
         InlineKeyboardButton(text="DETAILS", callback_data="Miko_"),
         InlineKeyboardButton(text="AI", callback_data="ai_handler"),
-        
+        InlineKeyboardButton(text="GROUPS", callback_data="@gcanimecommunity"),
     ],
     [
-        InlineKeyboardButton(text="OWNER", url=f"tg://user?id={OWNER_ID}"),
+        InlineKeyboardButton(text="CREATOR", url=f"tg://user?id={OWNER_ID}"),
     ],
 ]
 
@@ -65,18 +65,18 @@ GROUP_START_BTN = [
     ],
     [
         InlineKeyboardButton(text="SUPPORT", url=f"https://t.me/{SUPPORT_CHAT}"),
-        InlineKeyboardButton(text="OWNER", url=f"tg://user?id={OWNER_ID}"),
+        InlineKeyboardButton(text="CREATOR", url=f"tg://user?id={OWNER_ID}"),
     ],
 ]
 
 ALIVE_BTN = [
     [
-        ib(text="UPDATES", url="https://t.me/fubuki_supports"),
-        ib(text="SUPPORT", url="https://t.me/rg_anime_group"),
+        ib(text="UPDATES", url="https://t.me/guess_update"),
+        ib(text="SUPPORT", url="https://t.me/gcanimecommunity"),
     ],
     [
         ib(
-            text="➕ ADD ME ➕",
+            text="⇦ ADD ME ⇨",
             url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
         ),
     ],
